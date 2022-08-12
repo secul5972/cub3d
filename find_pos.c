52de25750654af22e894075afa85835fbd3fbaba
Seungchul Oh <seungcoh@c5r7s5.42seoul.kr>
2022-08-12 14:54:55 +0900

    map start pos fix

--- find_pos.c
+++ find_pos.c
@@ -6,16 +6,16 @@
 /*   By: seungcoh <seungcoh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 14:22:00 by seungcoh          #+#    #+#             */
-/*   Updated: 2022/08/11 15:07:02 by seungcoh         ###   ########.fr       */
+/*   Updated: 2022/08/12 14:54:34 by seungcoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	fix_map_find_pos(t_cub3d_data *cub)
+void fix_map_find_pos(t_cub3d_data *cub)
 {
-	int	i;
-	int	j;
+	int i;
+	int j;
 
 	i = 0;
 	while (i < cub->m_height)
@@ -23,17 +23,32 @@ void	fix_map_find_pos(t_cub3d_data *cub)
 		j = 0;
 		while (j < cub->m_width)
 		{
-			if (cub->map[i][j] != '1' && cub->map[i][j] != '0'\
-			&& cub->map[i][j] != ' ')
+			if (cub->map[i][j] != '1' && cub->map[i][j] != '0' && cub->map[i][j] != ' ')
 				make_vec(&cub->cpos, j, i);
 			if (cub->map[i][j] == 'N')
+			{
 				make_vec(&cub->cdir, 0, -1);
+				cub->map[i][j] = '0';
+				continue;
+			}
 			if (cub->map[i][j] == 'S')
+			{
 				make_vec(&cub->cdir, 0, 1);
+				cub->map[i][j] = '0';
+				continue;
+			}
 			if (cub->map[i][j] == 'W')
+			{
 				make_vec(&cub->cdir, -1, 0);
+				cub->map[i][j] = '0';
+				continue;
+			}
 			if (cub->map[i][j] == 'E')
+			{
 				make_vec(&cub->cdir, 1, 0);
+				cub->map[i][j] = '0';
+				continue;
+			}
 			j++;
 		}
 		i++;
